8884e99fc1c898f4c291e3e284fb1926b67eb475
ashwin1-dev <root@localhost.localdomain>
2024-04-15 15:43:53 +0000

    my vlc jar file

---
+++ src/main/java/com/mydigital/prj1/App.java
@@ -0,0 +1,16 @@
+package com.mydigital.prj1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello India!" );
+        System.out.println( "Hello Maharashtra!" );
+        System.out.println( "Hello Nagpur" );
+    }
+}
